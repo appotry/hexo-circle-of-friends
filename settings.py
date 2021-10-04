1eb7b1244fae29e496257e48ed10143c7dbf2b36
appotry <andycrusoe@gmail.com>
2021-10-04 14:43:32 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -56,8 +56,8 @@ RETRY_MAX = 3
 GITHUB_FRIENDS_LINKS = {
     "enable": False,    # True 开启gitee issue兼容
     "type": "normal",  # volantis用户请在这里填写 volantis
-    "owner": "Zfour",  # 填写你的github用户名
-    "repo": "zfour-friends",  # 填写你的github仓库名
+    "owner": "appotry",  # 填写你的github用户名
+    "repo": "hexo-circle-of-friends",  # 填写你的github仓库名
     "state": "open"  # 填写抓取的issue状态(open/closed)
 }
 
